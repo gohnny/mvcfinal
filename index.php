835e084e012028e7a8d526362da69570fbf689a9
gohnny <gohnny@gmail.com>
2018-04-10 23:25:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-//формат даты  день-месяц- год
-//$string = '21-11-2019';
-//
-////Строка приведенного типа Год 2019 месяц 11 день 21;
-//
-//
-//$pattern='/([0-9]{2})-([0-9]{2})-([0-9]{4})/';
-//
-//$replasment= 'Год $3, месяц $2, день $1';
-//        
-//echo preg_replace($pattern, $replasment, $string);
-//die;
- //Mont 11 Day 21 Year 2015
- //
- //
- //
-//$string = '21-11-2015';
-//$pattern ='([0-9]{2})-([0-9]{2})-([0-9]{4}) ';
-//$replasment= 'Month $2 Day $1 Year$3';
-//echo preg_replace($pattern, $replasment, $string);
-//die;
-
-
 //  FRONT CONTORLLER
  
 // Общие настройки 
@@ -32,17 +9,6 @@ error_reporting(E_ALL);
 define('ROOT',__DIR__);
 require_once (ROOT.'/components/Router.php');
 require_once (ROOT.'/components/Db.php');
-//print_r(ROOT);
-
-//$string = '21-11-2015';
-//$pattern ='/([0-9]{2})-([0-9]{2})-([0-9]{4})/';
-//$replasment= 'Month $2 Day $1 Year$3';
-//echo preg_replace($pattern, $replasment, $string);
-
-
-
-
-
 
 // Вызов Router
 $router= new Router();
